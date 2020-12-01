219b83d0b8ef423838f5952af1bf31e67006a6ee
daniel <developmentleviathan@gmail.com>
2020-12-01 08:24:45 -0500

    correct index check

--- 1.kt
+++ 1.kt
@@ -3,18 +3,21 @@ import java.io.File
 fun main() {
     val vals = mutableListOf<Int>()
     val ab = HashMap<Int, Int>()
+    var p1 = 0
 
     File("input/1").forEachLine { vals.add(it.toInt()) }
 
-    loop@ for (a in vals)
-        for (b in vals) {
-            ab.put(a + b, a * b)
+    for ((ia, a) in vals.withIndex())
+        for ((ib, b) in vals.withIndex())
+            if (ia != ib) {
+                ab.put(a + b, a * b)
 
-            if (a + b == 2020) {
-                println("P1: " + a * b)
-                break@loop
+                if(a + b == 2020) {
+                    p1 = a * b;
+                }
             }
-        }
+
+    println("P1: " + p1)
 
     for (c in vals) {
         val prod = ab.get(2020 - c)

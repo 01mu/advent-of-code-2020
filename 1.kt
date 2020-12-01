d12fe505134cbaf4d75735ac5f24c110eabafcaa
daniel <developmentleviathan@gmail.com>
2020-12-01 05:41:05 -0500

    refractor 1

--- 1.kt
+++ 1.kt
@@ -2,26 +2,25 @@ import java.io.File
 
 fun main() {
     val vals = mutableListOf<Int>()
-    val hm = HashMap<Int, Int>()
+    val ab = HashMap<Int, Int>()
 
     File("input/1").forEachLine { vals.add(it.toInt()) }
 
-    loop@ for(a in vals)
-        for(b in vals) {
-            hm.put(a + b, a * b)
+    loop@ for (a in vals)
+        for (b in vals) {
+            ab.put(a + b, a * b)
 
-            if(a + b == 2020) {
+            if (a + b == 2020) {
                 println("P1: " + a * b)
                 break@loop
             }
         }
 
-    for(c in vals) {
-        val ab = 2020 - c
-        val g = hm.get(ab)
+    for (c in vals) {
+        val prod = ab.get(2020 - c)
 
-        if(g != null && ab + c == 2020) {
-            println("P2: " + g * c)
+        if (prod != null && 2020 - c + c == 2020) {
+            println("P2: " + prod * c)
             break
         }
     }

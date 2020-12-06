878985111a02a0ce6cee8e0e5646ed6201570658
daniel <developmentleviathan@gmail.com>
2020-12-05 19:02:26 -0500

    refractor

--- 5.kt
+++ 5.kt
@@ -3,33 +3,22 @@ import kotlin.math.*
 import kotlin.collections.*
 
 fun main() {
-    val input = mutableListOf<String>()
-    var list = mutableListOf<Int>()
-    var p2 = 0
-
-    File("input/5").forEachLine { input.add(it) }
-
-    for (dir in input) {
-        val row = findMin(dir.substring(0, 7), 'F', 127) * 8
-        val col = findMin(dir.substring(7), 'L', 7)
-
-        list.add(row + col)
-    }
-
-    val s = list.sorted()
-
-    for ((i, v) in s.withIndex()) if(i > 0 && (s[i - 1] != v - 1)) p2 = v - 1
-
-    println("P1: " + list.maxOrNull() + "\nP2: " + p2)
+    var ids = mutableListOf<Int>()
+    for (dir in File("input/5").readText().trim().split("\n"))
+        ids.add(getLower(dir, 'F', 127) * 8 + getLower(dir, 'L', 7))
+    val sorted = ids.sorted()
+    println("P1: " + sorted.last() + "\nP2: " + getUserID(sorted))
 }
 
-fun findMin(str: String, alt: Char, def: Int): Int {
+fun getLower(dir: String, alt: Char, def: Int): Int {
     var l = 0
     var h = def
+    for (ch in if (alt == 'F') dir.substring(0, 7) else dir.substring(7))
+        if (ch == alt) h = l + (h - l) / 2 else l = (h + l + 1) / 2
+    return l
+}
 
-    for (ch in str)
-        if (ch == alt) h = l + ((h - l) / 2)
-        else l = (h + l + 1) / 2
-
-    return min(l, h)
+fun getUserID(l: List<Int>): Int {
+    for ((i, v) in l.withIndex()) if (i > 0 && (l[i - 1] != v - 1)) return v - 1
+    return -1
 }
